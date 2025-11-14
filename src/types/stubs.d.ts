d38f76ff0a92565f793a777753646825cf815209
razedesoare <biancapatrusel@yahoo.com>
2025-11-14 19:24:20 +0200

    uppdates

---
+++ src/types/stubs.d.ts
@@ -0,0 +1,346 @@
+// TypeScript declarations for stubbed packages
+
+declare module 'lucide-react' {
+  import * as React from 'react';
+  export const ChevronDownIcon: React.ForwardRefExoticComponent<any>;
+  export const CheckIcon: React.ForwardRefExoticComponent<any>;
+  export const XIcon: React.ForwardRefExoticComponent<any>;
+  export const ChevronLeft: React.ForwardRefExoticComponent<any>;
+  export const ChevronRight: React.ForwardRefExoticComponent<any>;
+  export const ArrowLeft: React.ForwardRefExoticComponent<any>;
+  export const ArrowRight: React.ForwardRefExoticComponent<any>;
+  export const PanelLeftIcon: React.ForwardRefExoticComponent<any>;
+  export const MinusIcon: React.ForwardRefExoticComponent<any>;
+  export const SearchIcon: React.ForwardRefExoticComponent<any>;
+  export const CircleIcon: React.ForwardRefExoticComponent<any>;
+  export const ChevronRightIcon: React.ForwardRefExoticComponent<any>;
+  export const MoreHorizontal: React.ForwardRefExoticComponent<any>;
+  export const GripVerticalIcon: React.ForwardRefExoticComponent<any>;
+}
+
+declare module '@radix-ui/react-accordion' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const Header: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-alert-dialog' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Action: React.ComponentType<any>;
+  export const Cancel: React.ComponentType<any>;
+  export const Title: React.ComponentType<any>;
+  export const Description: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-aspect-ratio' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-avatar' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Image: React.ComponentType<any>;
+  export const Fallback: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-checkbox' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-collapsible' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-context-menu' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const CheckboxItem: React.ComponentType<any>;
+  export const RadioItem: React.ComponentType<any>;
+  export const Label: React.ComponentType<any>;
+  export const Separator: React.ComponentType<any>;
+  export const Sub: React.ComponentType<any>;
+  export const SubContent: React.ComponentType<any>;
+  export const SubTrigger: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-dialog' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Portal: React.ComponentType<any>;
+  export const Overlay: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Header: React.ComponentType<any>;
+  export const Footer: React.ComponentType<any>;
+  export const Title: React.ComponentType<any>;
+  export const Description: React.ComponentType<any>;
+  export const Close: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-dropdown-menu' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const CheckboxItem: React.ComponentType<any>;
+  export const RadioItem: React.ComponentType<any>;
+  export const Label: React.ComponentType<any>;
+  export const Separator: React.ComponentType<any>;
+  export const Sub: React.ComponentType<any>;
+  export const SubContent: React.ComponentType<any>;
+  export const SubTrigger: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-hover-card' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Portal: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-label' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-menubar' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Menu: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const CheckboxItem: React.ComponentType<any>;
+  export const RadioItem: React.ComponentType<any>;
+  export const Label: React.ComponentType<any>;
+  export const Separator: React.ComponentType<any>;
+  export const Sub: React.ComponentType<any>;
+  export const SubContent: React.ComponentType<any>;
+  export const SubTrigger: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-navigation-menu' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const List: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Link: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-popover' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Portal: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Anchor: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-progress' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Indicator: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-radio-group' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const Indicator: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-scroll-area' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Viewport: React.ComponentType<any>;
+  export const Scrollbar: React.ComponentType<any>;
+  export const Corner: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-select' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Group: React.ComponentType<any>;
+  export const Value: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Viewport: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+  export const ItemText: React.ComponentType<any>;
+  export const ItemIndicator: React.ComponentType<any>;
+  export const Label: React.ComponentType<any>;
+  export const Separator: React.ComponentType<any>;
+  export const Arrow: React.ComponentType<any>;
+  export const ScrollUpButton: React.ComponentType<any>;
+  export const ScrollDownButton: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-separator' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-slider' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Track: React.ComponentType<any>;
+  export const Range: React.ComponentType<any>;
+  export const Thumb: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-switch' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Thumb: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-tabs' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const List: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-toggle' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-toggle-group' {
+  import * as React from 'react';
+  export const Root: React.ComponentType<any>;
+  export const Item: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-tooltip' {
+  import * as React from 'react';
+  export const Provider: React.ComponentType<any>;
+  export const Root: React.ComponentType<any>;
+  export const Trigger: React.ComponentType<any>;
+  export const Portal: React.ComponentType<any>;
+  export const Content: React.ComponentType<any>;
+  export const Arrow: React.ComponentType<any>;
+}
+
+declare module '@radix-ui/react-slot' {
+  import * as React from 'react';
+  export const Slot: React.ForwardRefExoticComponent<any>;
+}
+
+declare module 'embla-carousel-react' {
+  import * as React from 'react';
+  type UseEmblaCarouselType = [React.RefObject<any>, any];
+  function useEmblaCarousel(...args: any[]): UseEmblaCarouselType;
+  export default useEmblaCarousel;
+  export type UseEmblaCarouselType = UseEmblaCarouselType;
+}
+
+declare module 'cmdk' {
+  import * as React from 'react';
+  export const Command: React.ComponentType<any>;
+  export const CommandDialog: React.ComponentType<any>;
+  export const CommandInput: React.ComponentType<any>;
+  export const CommandList: React.ComponentType<any>;
+  export const CommandEmpty: React.ComponentType<any>;
+  export const CommandGroup: React.ComponentType<any>;
+  export const CommandItem: React.ComponentType<any>;
+  export const CommandSeparator: React.ComponentType<any>;
+  export const CommandShortcut: React.ComponentType<any>;
+}
+
+declare module 'input-otp' {
+  import * as React from 'react';
+  export const OTPInput: React.ComponentType<any>;
+  export const OTPInputContext: React.Context<any>;
+}
+
+declare module 'react-day-picker' {
+  import * as React from 'react';
+  export const DayPicker: React.ComponentType<any>;
+  export default any;
+}
+
+declare module 'recharts' {
+  import * as React from 'react';
+  export const LineChart: React.ComponentType<any>;
+  export const Line: React.ComponentType<any>;
+  export const XAxis: React.ComponentType<any>;
+  export const YAxis: React.ComponentType<any>;
+  export const CartesianGrid: React.ComponentType<any>;
+  export const Tooltip: React.ComponentType<any>;
+  export const Legend: React.ComponentType<any>;
+  export const ResponsiveContainer: React.ComponentType<any>;
+  export const BarChart: React.ComponentType<any>;
+  export const Bar: React.ComponentType<any>;
+  export const PieChart: React.ComponentType<any>;
+  export const Pie: React.ComponentType<any>;
+  export const Cell: React.ComponentType<any>;
+}
+
+declare module 'class-variance-authority' {
+  export function cva(...args: any[]): any;
+  export type VariantProps<T> = any;
+}
+
+declare module 'react-hook-form' {
+  import * as React from 'react';
+  export function useForm(...args: any[]): any;
+  export function useFormContext(): any;
+  export function FormProvider(...args: any[]): React.ComponentType<any>;
+  export const Controller: React.ComponentType<any>;
+  export const useFieldArray: any;
+  export const useWatch: any;
+}
+
+declare module 'sonner' {
+  import * as React from 'react';
+  export const Toaster: React.ComponentType<any>;
+  export const toast: any;
+  export type ToasterProps = any;
+}
+
+declare module 'react-resizable-panels' {
+  import * as React from 'react';
+  export const Panel: React.ComponentType<any>;
+  export const PanelGroup: React.ComponentType<any>;
+  export const PanelResizeHandle: React.ComponentType<any>;
+}
+
+declare module 'vaul' {
+  import * as React from 'react';
+  export const Drawer: React.ComponentType<any>;
+  export const DrawerPortal: React.ComponentType<any>;
+  export const DrawerOverlay: React.ComponentType<any>;
+  export const DrawerTrigger: React.ComponentType<any>;
+  export const DrawerClose: React.ComponentType<any>;
+  export const DrawerContent: React.ComponentType<any>;
+  export const DrawerHeader: React.ComponentType<any>;
+  export const DrawerFooter: React.ComponentType<any>;
+  export const DrawerTitle: React.ComponentType<any>;
+  export const DrawerDescription: React.ComponentType<any>;
+}
+
+declare module 'next-themes' {
+  import * as React from 'react';
+  export function useTheme(): any;
+  export const ThemeProvider: React.ComponentType<any>;
+}
